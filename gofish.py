fdaf243a697355bcc558fabcd92b842e5b551971
Corey Schmierer <coreypersonal@Coreys-MacBook-Pro.local>
2023-04-09 10:41:11 +1000

    Added shuffle, deal function

--- gofish.py
+++ gofish.py
@@ -1,3 +1,5 @@
+import random
+
 class Card_Pack:
     def __init__(self) -> None:
         self.suits = ["C", "S", "H", "D"]
@@ -7,6 +9,17 @@ class Card_Pack:
         for suits in self.suits:
             for numbers in self.numbers:
                 self.draw_pack.append(Card(suits, numbers))
+    
+    def shuffle(self):
+        self.staging_pack = []
+        end_point = 51 
+        while len(self.staging_pack) != 52:
+            self.staging_pack.append(self.draw_pack.pop(random.randint(0, end_point)))
+            end_point -= 1
+        self.draw_pack = self.staging_pack
+    
+
+        
 
 class Card:
     def __init__(self, suit, number) -> None:
@@ -17,14 +30,16 @@ class Card:
         print(f"The card is a {self.number} of {self.suit}")
         return
 
-card_pack = Card_Pack()
+# card_pack = Card_Pack()
 
 class Go_Fish:
     def __init__(self) -> None:
         self.round = 0
         self.team_1_fours = []
         self.team_2_fours = []
-        self.draw_pack = Card_Pack()
+        self.card_pack = Card_Pack()
+        self.player_1 = Player(1, "Corey")
+        self.player_2 = Player(2, "Courteney")
     
     def four_check(self, team_1_hand, team_2_hand):
         hands = [team_1_hand, team_2_hand]
@@ -37,13 +52,32 @@ class Go_Fish:
                 if number_tracker(cards) == 4:
                     index_tracker.append(cards)
     
+    def deal(self):
+        return
+
+    def give_card(self, target_player):
+        target_player.hand.append(self.card_pack.draw_pack.pop(0))
+        return
     
+    def deal(self, starting_number_of_cards):
+        for i in range(starting_number_of_cards):
+            self.give_card(self.player_1)
+            self.give_card(self.player_2)
+        
+
+
+    def start_the_game(self, starting_hand_count):
+        self.card_pack.shuffle()
+        self.deal(starting_hand_count)
+        # self.give_card(self.player_1)
+        self.player_1.print_hand()
 
         
 class Player:
-    def __init__(self, number) -> None:
+    def __init__(self, number, name) -> None:
         self.number = number
-        self.hand = [Card("H", 3), Card("C", 3), Card("C", 6)] # made of cards
+        self.hand = [] # made of cards
+        self.name = name
 
     def place_fours_down(self, card_suit):
         for cards in self.hand:
@@ -70,13 +104,18 @@ class Player:
     
     def print_hand(self):
         for cards in self.hand:
-            print(cards.whats_the_card())
-        
+            cards.whats_the_card()
+    
+    def whats_my_name(self):
+        print(self.name)
 
 
 
-me = Player(1)
-me.print_hand()
-me.remove_card(3)
-print("hellooo")
-me.print_hand()
+# me = Player(1, "Corey")
+# me.print_hand()
+# me.remove_card(3)
+# print("hellooo")
+# me.print_hand()
+
+game = Go_Fish()
+game.start_the_game(7)
